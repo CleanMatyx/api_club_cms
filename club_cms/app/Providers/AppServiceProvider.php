0ba6a67f6d81fa39ad57c2164020f97599ad7cde
CleanMatyx <mtsbrr07@gmail.com>
2025-05-21 04:34:01 +0200

    Actualización de la configuración de expiración de tokens de Passport.

--- club_cms/app/Providers/AppServiceProvider.php
+++ club_cms/app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\Passport;
-use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
